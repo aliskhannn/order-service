88f4f67a5f23a65e97d4956af9c01abecea5ad71
Aliskhan <aliskhanreventon@gmail.com>
2025-07-03 11:07:35 -0400

    add chi cors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"github.com/go-chi/cors"
 	"net/http"
 	"os"
 	"os/signal"
@@ -54,6 +55,13 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+	}))
 
 	r.Get("/orders/{id}", orderGetHandler.GetOrderByID)
 
